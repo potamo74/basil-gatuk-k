5e7de64812c48a3af4b9b9e203aae87dc8412ab1
caral943 <caral943@student.liu.se>
2026-06-29 11:18:46 +0200

    Nya tider

--- script.js
+++ script.js
@@ -170,11 +170,11 @@ function updateTodayHours() {
 
   const hoursByDay = {
     0: { label: "Söndag", text: "Stängt", closed: true },
-    1: { label: "Måndag", text: "11:00 – 20:30" },
-    2: { label: "Tisdag", text: "11:00 – 20:30" },
-    3: { label: "Onsdag", text: "11:00 – 20:30" },
-    4: { label: "Torsdag", text: "11:00 – 20:30" },
-    5: { label: "Fredag", text: "11:00 – 20:30 | 23:00 – 03:30" },
+    1: { label: "Måndag", text: "Stängt", closed: true },
+    2: { label: "Tisdag", text: "11:00 – 19:00" },
+    3: { label: "Onsdag", text: "11:00 – 19:00" },
+    4: { label: "Torsdag", text: "11:00 – 19:00" },
+    5: { label: "Fredag", text: "11:00 – 19:00 | 23:00 – 03:30" },
     6: { label: "Lördag", text: "23:00 – 03:30" },
   };
 
